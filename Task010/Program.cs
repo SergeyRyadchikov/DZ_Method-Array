9201bd55cef18e15205f779d2cc6df62fbafe252
Sergey Ryadchikov <ryadchikov.sa@mail.ru>
2022-07-14 22:54:50 +0300

    add Task010_ver2

---
+++ Task010/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число  ");
+string number = Console.ReadLine();
+
+char[] array = new Char[number.Length];
+int index = 0;
+while (index < number.Length)
+{
+    array[index] = number[index];
+    index++;
+}
+foreach  (char num in array)                                                   
+Console.Write(num + "; ");
+Console.WriteLine();
+
+Console.Write(array[1]);
+
+
---
+++ Task010_ver2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число  ");
+string number = Console.ReadLine();
+
+int lenght = number.Length;
+char[] num = new Char[lenght];
+num = number.ToCharArray(0, lenght);
+Console.WriteLine(num);
+Console.Write("Вторая цифра - " + num[1]);
